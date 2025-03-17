7e68822bba7d7581d3c74054c3a0409a10caded5
Boyan Kloosterman <kloostermanboyan@gmail.com>
2025-03-17 12:59:14 +0100

    Database toegevoegd + packages

---
+++ ZorgAppAPI/Models/Arts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZorgAppAPI.Models
+{
+    public class Arts
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Naam { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Specialisatie { get; set; }
+
+        // Navigation properties
+        public virtual ICollection<Patient> Patienten { get; set; }
+    }
+}
---
+++ ZorgAppAPI/Models/OuderVoogd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZorgAppAPI.Models
+{
+    public class OuderVoogd
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Voornaam { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Achternaam { get; set; }
+
+        // Navigation properties
+        public virtual ICollection<Patient> Patienten { get; set; }
+    }
+}
