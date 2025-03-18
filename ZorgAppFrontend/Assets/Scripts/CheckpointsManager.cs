e19bc37b390fa22a50b4314f212e4741b817289c
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2025-03-18 14:19:17 +0100

    Created basic tijdlijn demonstration

---
+++ ZorgAppFrontend/Assets/Scripts/CheckpointsManager.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckpointSceneManager : MonoBehaviour
+{
+    public TextMeshProUGUI checkpointText;
+    private void Start()
+    {
+        Debug.Log($"Aangekomen bij checkpoint! ID: {RoutesManager.Instance.checkpointID}, Route: {RoutesManager.Instance.routeName}");
+        checkpointText.text = $"Route: {RoutesManager.Instance.routeName}, Checkpoint {RoutesManager.Instance.checkpointID}.";
+        //hier komt ook de api call groetjes pluk!
+    }
+
+    public void ReturnToRouteScene()
+    {
+        SceneManager.LoadScene(RoutesManager.Instance.routeName);
+    }
+}
